90006381d8ea5cb73b550e38252e250aa7baaf98
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-05 08:41:30 +0400

    Create for_file.py

---
+++ .github/python/for_file.py
@@ -0,0 +1,12 @@
+async def pp(file, n):
+    pFile = file
+    X = pFile.url
+    Y = pFile.data
+    if n != 10:
+        return pFile
+        return X
+        return Y
+
+for i in range(infinity):
+    pp(ChessForAll/python/ai.py, i)
+    print(pFile, X, Y)
